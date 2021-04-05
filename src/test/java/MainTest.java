22bec49639aa691ecb5ede2fd993a0bb8c2c716b
Kinga Borkowska <kinga.borkowska@best.com.pl>
2021-04-05 20:09:28 +0200

    initial commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MainTest {
+    @Test
+    public void test() {
+        assertThat(1).isEqualTo(1);
+    }
+
+}

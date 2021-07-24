7a16612fd6abe1cd1b19c4f96bff31d4d97e82f6
Kinga Borkowska <kinga.borkowska@best.com.pl>
2021-07-24 12:23:32 +0200

    refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,13 @@ public class Main {
     public int maxProfit(int[] prices) {
         if(prices.length == 1)   return 0;
 
+        return findMaxProfit(prices, 0, prices.length-1);
+    }
+
+    private int findMaxProfit(int[] prices, int start, int end) {
         int maxProfit = 0;
-        int start=0;
-        int end = prices.length-1;
-        for (int i = start+1; i <= end; i++) {
-            int profit = prices[i] - prices[0];
+        for (int rightIndex = start +1; rightIndex <= end; rightIndex++) {
+            int profit = prices[rightIndex] - prices[start];
             if(profit > maxProfit)
                 maxProfit = profit;
         }

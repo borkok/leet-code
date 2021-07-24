1d91019cf684097959a0c1328df5ae201e4f4a61
Kinga Borkowska <kinga.borkowska@best.com.pl>
2021-07-24 12:01:08 +0200

    refactor: parametrized test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,11 +1,22 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MainTest {
-    @Test
-    public void test() {
-        int profit = new Main().maxProfit(new int[] { 1 });
-        assertThat(profit).isEqualTo(0);
+    private static Stream<Arguments> params() {
+        return Stream.of(
+                Arguments.of(new int[]{1}, 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("params")
+    public void test(int[] prices, int expected) {
+        int profit = new Main().maxProfit(prices);
+        assertThat(profit).isEqualTo(expected);
     }
 }

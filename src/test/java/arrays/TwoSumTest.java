910b097feea8c9c895025ddc2ed3bc556bd4776c
Kinga Borkowska <kinga.borkowska@best.com.pl>
2021-07-28 20:28:30 +0200

    green O(n2)

--- src/test/java/arrays/TwoSumTest.java
+++ src/test/java/arrays/TwoSumTest.java
@@ -16,19 +16,8 @@ class TwoSumTest {
                 ,Arguments.of(new int[]{1,2,1}, 2, new int[]{0,2})
                 ,Arguments.of(new int[]{1,1,2}, 2, new int[]{0,1})
                 ,Arguments.of(new int[]{2,1,1}, 2, new int[]{2,1})
-/*
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Output: Because nums[0] + nums[1] == 9, we return [0, 1].
-Example 2:
-
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-Example 3:
-
-Input: nums = [3,3], target = 6
-Output: [0,1]
- */
+                ,Arguments.of(new int[]{11,2,15,7}, 9, new int[]{1,3})
+                ,Arguments.of(new int[]{2,7,11,15}, 9, new int[]{0,1})
         );
     }
 

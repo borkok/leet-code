b98130435023c87d6596897fd1dc9762f9d1e855
Kinga Borkowska <kinga.borkowska@best.com.pl>
2021-07-28 09:53:17 +0200

    green

--- src/test/java/arrays/SolutionTest.java
+++ src/test/java/arrays/SolutionTest.java
@@ -65,8 +65,11 @@ class SolutionTest {
     private static Stream<Arguments> plusOne() {
         return Stream.of(
                 Arguments.of(new int[]{1}, new int[]{2})
+                ,Arguments.of(new int[]{1,9}, new int[]{2,0})
                 ,Arguments.of(new int[]{9}, new int[]{1,0})
+                ,Arguments.of(new int[]{9,9,9}, new int[]{1,0,0,0})
                 ,Arguments.of(new int[]{2,4,9}, new int[]{2,5,0})
+                ,Arguments.of(new int[]{9,8,7,6,5,4,3,2,1,0}, new int[]{9,8,7,6,5,4,3,2,1,1})
         );
     }
 

20119a7a95b63ac75cc9c2576bee678752a12236
Kinga Borkowska <kinga.borkowska@best.com.pl>
2021-07-28 21:17:58 +0200

    refactor exctract class

--- src/main/java/arrays/TwoSum.java
+++ src/main/java/arrays/TwoSum.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Given an array of integers nums and an integer target,
@@ -35,37 +36,74 @@ public class TwoSum {
         return result;
     }
 
-    // max 104 + 218 = 322 operations
+    // maxNumber 104 + 218 = 322 operations
     // but more memory
     private int[] linear(int[] nums, int target) {
-        // make hash map and find min and max
-        Map<Integer, List<Integer>> indexByNum = new HashMap<>();
-        int min = 110;
-        int max = -110;
-        for (int i = 0; i < nums.length; i++) {
-            indexByNum.merge(nums[i],
-                             new LinkedList<>(Collections.singletonList(i)),
-                             TwoSum::mergeLists);
-            min = Math.min(nums[i], min);
-            max = Math.max(nums[i], max);
-        }
-        // try all possible sums
+        // make hash map and find minNumber and maxNumber
+        NumberIndices numberIndices = new NumberIndices(nums);
+        int min = numberIndices.minNumber();
+        int max = numberIndices.maxNumber();
+
+        // try all possible pair of integers that sum to target
         for (int left = min; left < max; left++) {
             int right = target - left;
-            if (left == right) {
-                if (indexByNum.containsKey(left) && indexByNum.get(left).size() > 1) {
-                    return new int[] { indexByNum.get(left).get(0), indexByNum.get(left).get(1) };
-                }
-            }
-            if (indexByNum.containsKey(left) && indexByNum.containsKey(right)) {
-                return new int[] { indexByNum.get(left).get(0), indexByNum.get(right).get(0) };
-            }
+            Optional<Pair> indices = numberIndices.findIndicesOf(new Pair(left, right));
+            if (indices.isPresent()) {
+                return new int[] { indices.get().left, indices.get().right };
+            };
         }
         throw new IllegalArgumentException();
     }
 
+    private static class NumberIndices {
+        private final Map<Integer, List<Integer>> indexByNum;
+        private int min;
+        private int max;
 
-    //n2/2 => max 104^2 / 2 = 5 000 operations
+        NumberIndices(int[] nums) {
+            indexByNum = new HashMap<>();
+            min = 110;
+            max = -110;
+            for (int i = 0; i < nums.length; i++) {
+                indexByNum.merge(nums[i],
+                                 new LinkedList<>(Collections.singletonList(i)),
+                                 TwoSum::mergeLists);
+                min = Math.min(nums[i], min);
+                max = Math.max(nums[i], max);
+            }
+        }
+
+        int minNumber() {
+            return min;
+        }
+
+        int maxNumber() {
+            return max;
+        }
+
+        Optional<Pair> findIndicesOf(Pair numbers) {
+            if (numbers.left == numbers.right) {
+                if (indexByNum.containsKey(numbers.left) && indexByNum.get(numbers.left).size() > 1) {
+                    return Optional.of(new Pair(indexByNum.get(numbers.left).get(0), indexByNum.get(numbers.left).get(1)));
+                }
+            } else if (indexByNum.containsKey(numbers.left) && indexByNum.containsKey(numbers.right)) {
+                return Optional.of(new Pair(indexByNum.get(numbers.left).get(0), indexByNum.get(numbers.right).get(0)));
+            }
+            return Optional.empty();
+        }
+    }
+
+    private static class Pair {
+        int left;
+        int right;
+
+        public Pair(int left, int right) {
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    //n2/2 => maxNumber 104^2 / 2 = 5 000 operations
     private int[] quadratic(int[] nums, int target) {
         for (int left = 0; left < nums.length; left++) {
             for (int right = left + 1; right < nums.length; right++) {
@@ -76,4 +114,5 @@ public class TwoSum {
         }
         throw new IllegalArgumentException();
     }
+
 }

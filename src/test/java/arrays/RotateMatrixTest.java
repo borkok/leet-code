e8ddcf0ea86954b6de28822fbd7a0058848773a8
Kinga Borkowska <kinga.borkowska@best.com.pl>
2021-08-05 19:27:43 +0200

    green

---
+++ src/test/java/arrays/RotateMatrixTest.java
@@ -0,0 +1,26 @@
+package arrays;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RotateMatrixTest {
+    private static Stream<Arguments> rotate() {
+        return Stream.of(
+                //1x1
+                Arguments.of(new int[][]{{1}}, new int[][]{{1}})
+         );
+    }
+
+    @ParameterizedTest
+    @MethodSource("rotate")
+    public void rotate(int[][] matrix, int[][] expected) {
+        new RotateMatrix().rotate(matrix);
+        assertThat(matrix).isDeepEqualTo(expected);
+    }
+
+}

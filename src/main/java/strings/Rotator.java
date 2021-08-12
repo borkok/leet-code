362bacdff8ea2a324c72d470fbc68c03709a746f
Kinga Borkowska <kinga.borkowska@best.com.pl>
2021-08-12 08:50:38 +0200

    red

---
+++ src/main/java/strings/Rotator.java
@@ -0,0 +1,11 @@
+package strings;
+
+/*
+1 <= s.length <= 105
+s[i] is a printable ascii character.
+ */
+public class Rotator {
+    public void reverseString(char[] s) {
+
+    }
+}
---
+++ src/test/java/strings/RotatorTest.java
@@ -0,0 +1,25 @@
+package strings;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RotatorTest {
+    private static Stream<Arguments> params() {
+        return Stream.of(
+                Arguments.of(new char[]{'a'}, new char[]{'a'}),
+                Arguments.of(new char[]{'a','b'}, new char[]{'b','a'})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("params")
+    public void reverseString(char[] text, char[] expected) {
+        new Rotator().reverseString(text);
+        assertThat(text).containsExactly(expected);
+    }
+}

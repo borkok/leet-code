3409bd0da1f633fbba476634ce66833093522a9c
kinga <kinga.borkowska.srv@gmail.com>
2021-09-03 13:39:38 +0200

    ordered list - refactor

--- src/main/java/orderedList/Counter.java
+++ src/main/java/orderedList/Counter.java
@@ -5,7 +5,11 @@ public class Counter {
     private int counter2 = 0;
 
     String getNextNumber(int i) {
-        if (i == 2)  return String.valueOf(counter1) + "." + String.valueOf(++counter2);
+        if (i == 2)  return concatWithDot(String.valueOf(counter1), String.valueOf(++counter2));
         return String.valueOf(++counter1);
+    }
+
+    String concatWithDot(String... numbers) {
+        return String.join(".",numbers);
     }
 }

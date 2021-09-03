16cc80b7e9bcbb7ecbd99f5d194f6ee9aba2701e
kinga <kinga.borkowska.srv@gmail.com>
2021-09-03 13:17:40 +0200

    ordered list - first green

---
+++ src/main/java/orderedList/OrderedList.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2021. BEST S.A. and/or its affiliates. All rights reserved.
+ */
+package orderedList;
+
+import java.util.List;
+
+/*
+Given sequence of strings
+# wordone
+wordtwo
+# wordthree
+## wordfour
+## wordfive
+wordsix
+# wordseven
+## wordeight
+### wordnine
+### wordten
+
+replace hash sign with correct chapter number meaning, ignoring lines without hash:
+1 wordone
+2 wordtwo
+2.1 wordfour
+2.2 wordfive
+3 wordseven
+3.1 wordeight
+3.1.1 wordnine
+3.1.2 wordten
+ */
+public class OrderedList {
+    public List<String> format(List<String> lines) {
+        return List.of();
+    }
+}
---
+++ src/test/java/orderedList/OrderedListTest.java
@@ -0,0 +1,27 @@
+package orderedList;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import strings.ReverseInteger;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderedListTest {
+    private static Stream<Arguments> params() {
+        return Stream.of(
+                Arguments.of(List.of("word"), List.of())
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("params")
+    public void format(List<String> lines, List<String> expected) {
+        assertThat(new OrderedList().format(lines)).isEqualTo(expected);
+    }
+}
